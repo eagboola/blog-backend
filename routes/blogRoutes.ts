f55aec128fac4b547d7ef37eba9659d4e4b90f9f
eagboola <ekerin.m.a@gmail.com>
2026-02-25 08:15:16 -0600

    feat(blog): add REST API routes
    Matched to basic CRUD controllers.

---
+++ routes/blogRoutes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import * as blogController from '../controllers/blogController.ts';
+
+const router = Router();
+
+router.get('/', blogController.getAllBlogs);
+router.post('/', blogController.createBlog);
+router.get('/:id', blogController.getBlogById);
+router.patch('/:id', blogController.updateBlog);
+router.delete('/:id', blogController.deleteBlog);
+
+export default router;
